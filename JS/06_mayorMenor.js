75575d764e0757e214a1a7e619bdfa2b5a0b3fb1
Nacho <barrojojoaquin2@gmail.com>
2025-04-15 08:10:37 -0300

    Se creo una condicional donde se pide 2 números y se muestra el mayor

---
+++ JS/06_mayorMenor.js
@@ -0,0 +1,7 @@
+let num1= parseInt(prompt("Introduce el primer número:"));
+let num2= parseInt(prompt("Introduce el segundo número:"));
+if (num1 > num2){
+document.writeln("El número mayor es:" + num1);
+}else if (num2 > num1){
+    document.writeln("El número mayor es:" + num2);
+}
