2c58d8d6b95169e7239594c186ce5272ac8994f7
Nacho <barrojojoaquin2@gmail.com>
2025-04-16 10:26:58 -0300

    Se agrego una estructura donde introducimos un número y muestra todos por el cual es divisible

--- JS/11_numeroDivisibles.js
+++ JS/11_numeroDivisibles.js
@@ -1,14 +1,15 @@
 // Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
 
 let numero = parseInt(prompt("Introduce un número: "));
+document.writeln("El número introducido es: " + numero+ "<br>");
 if (numero % 2 === 0){
-    document.writeln("El número es divisible por 2.");
+    document.writeln("El número es divisible por: 2" + "<br>");
 }if (numero % 3 === 0){
-    document.writeln("El número es divisible por 3.");
+    document.writeln("El número es divisible por: 3" + "<br>");
 }
 if (numero % 5 === 0){
-    document.writeln("El número es divisible por 5.");
+    document.writeln("El número es divisible por: 5" + "<br>");
 }
 if (numero % 7 === 0){
-    document.writeln("El número es divisible por 7.");
+    document.writeln("El número es divisible por: 7" + "<br>");
 }
