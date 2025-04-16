1875269d1f8a4dfeed23420046b6209567a4fb3a
Nacho <barrojojoaquin2@gmail.com>
2025-04-16 09:52:43 -0300

    Se agrego una estructura donde indica por cual número es divisible segun el número ingresado

---
+++ JS/10_numeroDivisible.js
@@ -0,0 +1,15 @@
+// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+// Si no es divisible por ninguno de ellos, que lo indique también.
+
+let numero = parseInt(prompt("Introduce un número: "));
+if (numero % 2 === 0){
+    document.writeln("El número es divisible por 2.");
+}if (numero % 3 === 0){
+    document.writeln("El número es divisible por 3.");
+}
+if (numero % 5 === 0){
+    document.writeln("El número es divisible por 5.");
+}
+if (numero % 7 === 0){
+    document.writeln("El número es divisible por 7.");
+}
