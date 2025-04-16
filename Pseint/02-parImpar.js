22075bf447b96ca87c30324d66f54941142e794c
Nacho <barrojojoaquin2@gmail.com>
2025-04-16 17:07:37 -0300

    Se hizo una estructura para saber si un número es par o impar

---
+++ Pseint/02-parImpar.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Ingrese un número: "));
+if (numero % 2 == 0) {
+  document.writeln(`El número ${numero} es par:` );
+} else {
+  document.writeln(`El número ${numero} es impar`);
+}
